891b518be693909c64dd28f820532a2b0fa537a8
Antonino Francesco Musolino <ninomusolino@gmail.com>
2016-11-15 16:21:48 +0100

    Initial commit

---
+++ Example/AMDragDrop/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AMDragDrop
+//
+//  Created by Antonino Francesco Musolino on 11/15/2016.
+//  Copyright (c) 2016 Antonino Francesco Musolino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
